787ec0e0ebc60d5facc457b9c8c8292bd47c85b1
Rajeshwari Koppu <rajeshwarikoppu76@gmail.com>
2026-03-01 11:58:04 +0530

    core services

--- src/components/coreServices/coreServices.tsx
+++ src/components/coreServices/coreServices.tsx
@@ -1,21 +1,22 @@
 import twentyfour from "../../../public/coreServices/24_by_7.jpg"
 import boxicon from "../../../public/coreServices/boxicons_hospital.svg"
 import doctor from "../../../public/coreServices/fontisto_doctor.png"
-import lets from "../../../public/coreServices/lets-icons_order.png"
+import plus from "../../../public/coreServices/Vector.png"
+import search from "../../../public/coreServices/search.png"
 import Image from "next/image"
 export default function CoreService(){
     return(
         <div className="flex gap-8 xl:px-20 md:px-12 px-6">
             <Image src={twentyfour} alt="" width={4000*0.17} height={4000*0.17}/>
-            <div className="grid grid-cols-2 gap-y-12 gap-x-9">
+            <div className="grid grid-cols-2 gap-y- gap-x-12">
                {
                 coredata.map((items,ind)=>{
                     return(
                         <div className="flex gap-3 items-center" key={ind}>
                           <Image src={items.image} alt="" width={120*0.8} height={120*0.8}/>
-                          <div className="flex flex-col gap-3">
-                              <span>{items.title}</span>
-                              <span>{items.title}</span>
+                          <div className="flex flex-col gap-3 tracking-tight">
+                              <span className="text-[#7f7e7e] text-6xl font-semibold">{items.title}</span>
+                              <span className="text-[#a0a0a0] text-lg">{items.des}</span>
                           </div>
                         </div>
                     )
@@ -28,7 +29,7 @@ export default function CoreService(){
 const coredata=[
     {
         id:1,
-        image:boxicon,
+        image:plus,
         title:"24/7",
         des:"services"
     },
@@ -52,14 +53,14 @@ const coredata=[
     },
     {
         id:1,
-        image:boxicon,
-        title:"24/7",
-        des:"services"
+        image:search,
+        title:"speaific",
+        des:"search"
     },
     {
         id:2,
-        image:boxicon,
-        title:"250+",
-        des:"hospitals"
+        image:doctor,
+        title:"specialist",
+        des:"doctors"
     },
 ]
