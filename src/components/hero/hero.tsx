2b3e3d673325ff6985b44d4e48aaad8383904159
Rajeshwari Koppu <rajeshwarikoppu76@gmail.com>
2026-03-06 11:03:56 +0530

    little chances in hero

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -7,16 +7,16 @@ const font1=localFont({
 })
 export default function Hero(){
     return(
-        <div className={`bg-[#00264c] xl:px-20 md:px-12 px-6 tracking-tighter 
+        <div className={`bg-[#00264c] xl:px-20 md:px-12 px-6 tracking-tighter md:pb-6
            ${font1.className}`}>
             <Navbar/>
          <div className="flex max-md:flex-col items-center">
             <div className="flex-col ">
               <span className='text-white xl:text-6xl lg:text-5xl md:text-4xl sm:text-3xl text-2xl font-semibold mr-7 xl:leading-17 mt-8'>Tired of waiting for booking appointments for a doctor</span>
               <p className='opacity-65  md:mt-8 mt-4 text-lg text-white'>No more waiting, book the doctor first, then visit and save time</p>
-              <div className='flex gap-14  md:mt-8 mt-4 text-center font-medium text-[15px]'>
-                <div className='text-black bg-white rounded-lg py-1.5 px-3 text-center'>Book an appointment</div>
-                <div className='bg-[#0066cc] rounded-lg py-1.5 px-3 text-center h-fit'>Login/Signup</div>
+              <div className='flex sm:gap-14 gap-8 md:mt-8 mt-4 text-center font-medium text-[15px]'>
+                <div className='text-black bg-white rounded-lg py-1.5 px-3 text-center whitespace-nowrap'>Book an appointment</div>
+                <div className='bg-[#0066cc] rounded-lg py-1.5 px-3 text-center h-fit text-white'>Login/Signup</div>
               </div>
             </div>
             <Image src={phones} alt="" width={577*1.5} height={433*1.5}/>
