1aa31a00d03894afe2c823cf63a7383054fdbf3a
Rajeshwari Koppu <rajeshwarikoppu76@gmail.com>
2026-06-22 21:06:09 +0530

    hospital dahsboard changes

--- src/app/api/hospital/Allappoinments/route.ts
+++ src/app/api/hospital/Allappoinments/route.ts
@@ -1,46 +1,41 @@
-
 import { NextResponse } from "next/server";
 import sql from "@/lib/dbs";
 import jwt from "jsonwebtoken";
 import { cookies } from "next/headers";
 
-
 interface JwtPayload {
   id: string;
   role: string;
 }
-export async function POST(req:Request) {
-    const body=await req.json();
-    try{
-         const cookieStore = await cookies();
-               const token = cookieStore.get("token")?.value;
-        
-               if (!token) {
-              return NextResponse.json(
-                { message: "User not logged in" },
-                { status: 401 }
-              );
-            }
-        
-            const decoded = jwt.verify(
-              token,
-              process.env.JWT_SECRET!
-            ) as JwtPayload;
-        
-            console.log(decoded)
-        
-            // optional role check
-            if (decoded.role !== "hospital") {
-              return NextResponse.json(
-                { message: "Unauthorized access" },
-                { status: 403 }
-              );
-            }
-        const{choice}=body
-        let data;
-        
-        if(choice=="appointments"){
-        data = await sql`
+export async function POST(req: Request) {
+  const body = await req.json();
+  try {
+    const cookieStore = await cookies();
+    const token = cookieStore.get("token")?.value;
+
+    if (!token) {
+      return NextResponse.json(
+        { message: "User not logged in" },
+        { status: 401 },
+      );
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET!) as JwtPayload;
+
+    console.log(decoded);
+
+    // optional role check
+    if (decoded.role !== "hospital") {
+      return NextResponse.json(
+        { message: "Unauthorized access" },
+        { status: 403 },
+      );
+    }
+    const { choice } = body;
+    let data;
+
+    if (choice == "appointments") {
+      data = await sql`
      SELECT
     to_jsonb(a) AS appointment,
     jsonb_build_object(
@@ -55,16 +50,19 @@ JOIN doctors d
 WHERE a.hospital_id = ${decoded.id};
        
 `;
-        }
-        else if(choice=="doctors"){
-            data = await sql`
+    } else if (choice == "doctors") {
+      data = await sql`
        select * from doctors 
        where hospital_id=${decoded.id}
        
 `;
-        }
-        else if(choice=="today appoinments"){
-            data = await sql`
+    } else if (choice == "today appoinments") {
+      const date = new Date();
+
+      const Date_day = new Date(new Date(date).getTime() + 330 * 60 * 1000);
+      const today = Date_day.toISOString().split("T")[0];
+
+      data = await sql`
             SELECT
     a.*,
     d.name as Doctor_name
@@ -72,27 +70,27 @@ WHERE a.hospital_id = ${decoded.id};
   JOIN doctors d
     ON a.doctor_id = d.id
   WHERE a.hospital_id = ${decoded.id}
-  AND appointment_date = CURRENT_DATE
+  AND appointment_date = ${today}
        
        
 `;
-        }
-        else{
-            data = await sql`
+    } else {
+      data = await sql`
        select * from equipments 
        where hospital_id=${decoded.id}
        
 `;
-        }
-    if (data.length==0){
-        return NextResponse.json({message:[]},{status:200})
     }
-  
-        return NextResponse.json({message:data},{status:200})
+    if (data.length == 0) {
+      return NextResponse.json({ message: [] }, { status: 200 });
+    }
 
-    }catch(error){
-        console.log(error);
-       return  NextResponse.json({message:"inter server error"},{status:500})
-    }
-    
+    return NextResponse.json({ message: data }, { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      { message: "inter server error" },
+      { status: 500 },
+    );
+  }
 }
