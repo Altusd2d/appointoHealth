df23d7a70576bb4f2a9638f2a3571573f534ef07
Rajeshwari Koppu <rajeshwarikoppu76@gmail.com>
2026-06-23 22:41:17 +0530

    updated presnet appointments

--- src/app/api/user/presentAppointments/route.ts
+++ src/app/api/user/presentAppointments/route.ts
@@ -12,26 +12,25 @@ SELECT
     'name', d.name,
     'specialist', d.specialist,
     'experience', d.experience,
-    'education',d.education,
+    'education', d.education,
     'image', d.image
   ) AS doctor
 FROM appointments a
 JOIN doctors d
   ON a.doctor_id = d.id
 WHERE a.patient_id = ${user.userId}
-  AND a.status != 'cancelled'
+  AND a.status NOT IN ('cancelled', 'completed')
   AND (
     a.appointment_date::DATE >= CURRENT_DATE
     OR a.status = 'booked'
   );
-  AND a.status!='completed'
 `;
 
-// console.log("presentt appointmensts "+presentAppointments+"two details")
-// presentAppointments.forEach((appointment, index) => {
-//   console.log(`Appointment ${index + 1}:`, appointment);
-// });
-    return Response.json({ presentAppointments});
+    // console.log("presentt appointmensts "+presentAppointments+"two details")
+    // presentAppointments.forEach((appointment, index) => {
+    //   console.log(`Appointment ${index + 1}:`, appointment);
+    // });
+    return Response.json({ presentAppointments });
   } catch (error) {
     console.error(error);
     return Response.json({ message: "Unauthorized" }, { status: 401 });
